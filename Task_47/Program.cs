41d9d840ee80170b338c3ee92af8477390fa79a7
Maksim <maksim6erbinin@mail.ru>
2022-07-16 19:10:31 +0300

    Added task 47

---
+++ Task_47/Program.cs
@@ -0,0 +1,31 @@
+﻿double[,] CreateMatrix(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            matrix[i, k] = Convert.ToDouble(new Random().Next(-999, 1000)) / 100;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            Console.Write("{0:F2} \t", matrix[i, k]);
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите кол-во строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintMatrix(CreateMatrix(m, n));
